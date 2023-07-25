ffa70042231cd51b6556eb6646db7c81c8600643
LARRY MOSQUERA <larrysalsa@icloud.com>
2023-07-24 21:56:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>ESTA ES MI APLICACION DE LARRYSALSA EN REACT</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
